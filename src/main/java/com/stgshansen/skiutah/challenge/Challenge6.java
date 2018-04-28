3b5b4f776f91f31f0971393eb6af5a8bfac0e1f4
scott.hansen <scott.hansen@stgconsulting.com>
2018-04-28 12:34:25 -0600

    Added gecko driver.

--- src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
+++ src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
@@ -1,5 +1,6 @@
 package com.stgshansen.skiutah.challenge;
 
+import java.util.List;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -26,9 +27,10 @@ public class Challenge6 {
 	static PriorityQueue <String> leftToCrawlUrls = new PriorityQueue<String>();
 	static HttpURLConnection huc = null;
     static int respCode = 200;
+    static int index;
 	
 	public static void main(String[] args) throws Exception {
-	    System.setProperty("webdriver.gecko.driver", "C:/Dev/Tools/geckodriver.exe");
+	    System.setProperty("webdriver.gecko.driver", "src/geckodriver.exe");
 	    FirefoxDriver driver = new FirefoxDriver();
 	    //Opening SkiUtah.com
 	    driver.get(crawlingThisPage);
@@ -43,7 +45,7 @@ public class Challenge6 {
 	    driver.close();
 	}
 
-	@SuppressWarnings({ "unlikely-arg-type", "unchecked" })
+	@SuppressWarnings({ "unlikely-arg-type" })
 	private static void findUrls(String crawlingThisPage, String url, String xPathLinks,
 			PriorityQueue<WebElement> linksOnPage, PriorityQueue<String> foundOnPage,
 			PriorityQueue<String> allValidUrls, PriorityQueue<String> alreadyCrawledUrls,
@@ -51,8 +53,9 @@ public class Challenge6 {
 		
 
 		// Find links on this page.
-		PriorityQueue <WebElement> linksOnPage1 = (PriorityQueue<WebElement>) driver.findElementsByXPath(xPathLinks);	//Ensures the link has the domain in it.  Adds to linksOnPage
-        System.out.println(xPathLinks +" is being crawled.");
+		driver.get(crawlingThisPage);
+		List<WebElement> linksOnPage1 = driver.findElementsByXPath(xPathLinks);	//Ensures the link has the domain in it.  Adds to linksOnPage
+        System.out.println(crawlingThisPage +" is being crawled.");
 		
 		Iterator <WebElement> it = linksOnPage1.iterator();
 		while(it.hasNext()) {
@@ -66,7 +69,7 @@ public class Challenge6 {
 	                    System.out.println(url+" is a broken link");
 	                }
 	                else{
-	                    System.out.println(url+" is a valid link");
+//	                    System.out.println(url+" is a valid link");
 	                    foundOnPage.add(url);
 	                }
 	            } catch (MalformedURLException e) {
@@ -96,15 +99,29 @@ public class Challenge6 {
         leftToCrawlUrls.addAll(lhs);  // The left to be crawled list is created or added to.
         //leftToCrawlUrls.sort(null);
         lhs.clear();
+        System.out.println("Just cleared lhs and the size is now -" + lhs.size()); // This prints lhs is empty.
         lhs.addAll(leftToCrawlUrls);
+        System.out.println("Added all the left to crawl to lhs - size now - " + lhs.size()); // Added the left to crawl.
         lhs.remove(alreadyCrawledUrls);
+        for (index = 0; index < alreadyCrawledUrls.size(); ++index) {
+            lhs.remove(alreadyCrawledUrls.poll());
+        }  
+        System.out.println("Tried to remove Already crawled from lhs - " + lhs.size()); // Should check size after already crawled are removed.
         leftToCrawlUrls.clear();
         leftToCrawlUrls.addAll(lhs);  // The left to be crawled list is created or added to.
         System.out.println("This many URLs are in the queue to be crawled " +leftToCrawlUrls.size());
         System.out.println("Running Total of All URLs on Domain after the last was removed. " + leftToCrawlUrls); // This prints out the list of all found URLs so far on the domain.
-        System.out.println("Next URL to crawl - " + leftToCrawlUrls.poll()); // This prints out the list of all found URLs so far on the domain.
         leftToCrawlUrls.remove(0);
-         
+        System.out.println("Next URL to crawl - " + leftToCrawlUrls.poll()); // This prints out the list of all found URLs so far on the domain.
+
+        if (crawlingThisPage.equals(leftToCrawlUrls.poll())) {
+        	leftToCrawlUrls.remove(0);
+        	crawlingThisPage = leftToCrawlUrls.poll();
+            System.out.println("True Next Url to crawl - " + leftToCrawlUrls.poll()); // This prints out the next to crawl url.
+
+        }
+    	crawlingThisPage = leftToCrawlUrls.poll();
+
 	}
 	
 
