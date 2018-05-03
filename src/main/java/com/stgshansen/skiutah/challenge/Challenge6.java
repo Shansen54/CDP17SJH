8ef7af02f492215bff1534bd25295d65ce3e3256
scott.hansen <scott.hansen@stgconsulting.com>
2018-05-03 15:44:24 -0600

    Latest mash of previous and Matt's hint.

--- src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
+++ src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
@@ -1,129 +1,110 @@
 package com.stgshansen.skiutah.challenge;
 
-import java.util.List;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.PriorityQueue;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Challenge6 {
 
-	static String crawlingThisPage = "https://www.skiutah.com/"; // this is good and works.
-	static String homePage = "'https://www.skiutah.com/'";
-	static String xPathLinks = "//a[contains(@href, "+ homePage +")]";
-	static String url ="";
-	static PriorityQueue <WebElement> linksOnPage = new PriorityQueue<WebElement>();
-//	static Queue <WebElement> linksOnPage = null;
-//	static List <WebElement> linksOnPage = null;
-	static PriorityQueue <String> foundOnPage = new PriorityQueue<String>();
-	static PriorityQueue <String> allValidUrls = new PriorityQueue<String>();
-	static PriorityQueue <String> alreadyCrawledUrls = new PriorityQueue<String>();
-	static PriorityQueue <String> leftToCrawlUrls = new PriorityQueue<String>();
-	static HttpURLConnection huc = null;
-    static int respCode = 200;
+	static String homePage = "https://www.skiutah.com/";
+	static PriorityQueue<String> foundOnPage = new PriorityQueue<String>();
+	static String thisUrl = "";
+	static ArrayList <String> alreadyCrawledUrls = new ArrayList <String>();
+	static WebElement webEle = null;
+	static List<WebElement> webEles;
+	static LinkedHashSet<WebElement> SortedList = new LinkedHashSet<WebElement>();
     static int index;
 	
+	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) throws Exception {
 	    System.setProperty("webdriver.gecko.driver", "src\\geckodriver.exe");
 	    FirefoxDriver driver = new FirefoxDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
 	    //Opening SkiUtah.com
-	    driver.get(crawlingThisPage);
-	    
-	    findUrls(crawlingThisPage, url, xPathLinks, linksOnPage, foundOnPage,
-	    		allValidUrls, alreadyCrawledUrls, leftToCrawlUrls, huc, respCode, driver);
-	    
-	    while (leftToCrawlUrls != null) {
-	    		findUrls(crawlingThisPage, url, xPathLinks, linksOnPage, foundOnPage, allValidUrls, alreadyCrawledUrls, 
-	    		leftToCrawlUrls, huc, respCode, driver);
+	    driver.get(homePage);
+	    foundOnPage.add(homePage);
+ 
+	    while (foundOnPage != null) {
+	    	webEles = driver.findElements(By.tagName("a"));
+	    	webEle = webEles.get(index);
+			for(WebElement webEle : webEles) { SortedList.add(webEle);	}
+	    	webEles.removeAll(webEles);
+	    	webEles.addAll(SortedList);
+	    	webEles.remove(alreadyCrawledUrls);
+	    	
+	    	System.out.println("Total Links found on this crawled page -------------------------------------- " + webEles.size());
+			for(WebElement webEle : webEles) {	
+				if(webEle.getAttribute("href").contains("https://www.skiutah.com") && webEle.isDisplayed()) {
+					thisUrl = webEle.getAttribute("href");
+					System.out.println("Adding " + thisUrl + " to foundOnPage");
+					foundOnPage.add(thisUrl);
+					System.out.println("foundOnPage is now " + foundOnPage.size() + " size");
+				}
+ 	    }
+	    	goToUrl(alreadyCrawledUrls, foundOnPage, webEle, wait, driver);
 	    }
 	    driver.close();
 	}
 
-	@SuppressWarnings({ "unlikely-arg-type" })
-	private static void findUrls(String crawlingThisPage, String url, String xPathLinks,
-			PriorityQueue<WebElement> linksOnPage, PriorityQueue<String> foundOnPage,
-			PriorityQueue<String> allValidUrls, PriorityQueue<String> alreadyCrawledUrls,
-			PriorityQueue<String> leftToCrawlUrls, HttpURLConnection huc, int respCode, FirefoxDriver driver) {
-
-		// Find links on this page.
-		driver.get(crawlingThisPage);
-		List<WebElement> linksOnPage1 = driver.findElementsByXPath(xPathLinks);	//Ensures the link has the domain in it.  Adds to linksOnPage
-        System.out.println(crawlingThisPage +" is being crawled.");
-		
-		Iterator <WebElement> it = linksOnPage1.iterator();
-		while(it.hasNext()) {
-			url = it.next().getAttribute("href");
-	           try {
-	                huc = (HttpURLConnection)(new URL(url).openConnection());
-	                huc.setRequestMethod("HEAD");
-	                huc.connect();
-	                respCode = huc.getResponseCode();
-	                if(respCode >= 400){
-	                    System.out.println(url+" is a broken link");
-	                }
-	                else{
-//	                    System.out.println(url+" is a valid link");
-	                    foundOnPage.add(url);
-	                }
-	            } catch (MalformedURLException e) {
-	                e.printStackTrace();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-		
+	private static void goToUrl(ArrayList<String> alreadyCrawledUrls, PriorityQueue<String> foundOnPage, 
+			WebElement webEle, WebDriverWait wait, FirefoxDriver driver) {
+//		for(WebElement webEle : driver.findElements(By.tagName("a"))) {	
+//			wait.until(driver.findElementsByTagName() isTrue)
+			System.out.println("I am now going to this page - " + webEle.getAttribute("href"));
+//			if(webEle.getAttribute("href").contains("https://www.skiutah.com") && webEle.isDisplayed()) {
+				Actions action = new Actions(driver);
+				action.moveToElement(webEle).build().perform();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
 			
-		//(PriorityQueue<String> foundOnPage).sort(null);  // Sorts the valid Urls found on this page.
-        System.out.println("Total urls found on this page " + foundOnPage.size()); // Prints the total number of valid links found on this page.
-        LinkedHashSet <String> lhs = new LinkedHashSet<String>(); // Create the Linked Hash Set
-        lhs.addAll(foundOnPage);  // Adds all the links into the hash.  The duplicates are stripped as they come in.
-        foundOnPage.clear();  // Clears the ArrayList.
-        foundOnPage.addAll(lhs);  //  Adds all the hash entries back into the array.
-        System.out.println("Total urls with duplicates removed " + foundOnPage.size());  //  Prints the total number of unique valid with no duplicates
-        alreadyCrawledUrls.add(crawlingThisPage); // Adds the currently crawled page to the Array of crawled URLs.
-        System.out.println("This URL was crawled " + crawlingThisPage);  // Prints out the name of the crawled page.
-        System.out.println("Running total of Crawled sites " + alreadyCrawledUrls);  // Prints out the name of the crawled page.
-        allValidUrls.addAll(lhs);  // All valid urls get added to this list.
-        System.out.println("Running Total of All URLs on Domain " + allValidUrls); // This prints out the list of all found URLs so far on the domain. 
-        System.out.println("Running Size of All URLs on Domain " + allValidUrls.size()); // This prints out the size of all found URLs so far on the domain. 
-        lhs.remove(alreadyCrawledUrls);
-        System.out.println("This many URLs are in lhs after already crawled is taken out " + lhs.size());
-        System.out.println("Running Total of All Crawled Urls " + alreadyCrawledUrls); // This prints out the list of all crawled URLs so far on the domain. 
-        leftToCrawlUrls.addAll(lhs);  // The left to be crawled list is created or added to.
-        //leftToCrawlUrls.sort(null);
-        lhs.clear();
-        System.out.println("Just cleared lhs and the size is now -" + lhs.size()); // This prints lhs is empty.
-        lhs.addAll(leftToCrawlUrls);
-        System.out.println("Added all the left to crawl to lhs - size now - " + lhs.size()); // Added the left to crawl.
-        lhs.remove(alreadyCrawledUrls);
-        for (index = 0; index < alreadyCrawledUrls.size(); ++index) {
-            lhs.remove(alreadyCrawledUrls.poll());
-        }  
-        System.out.println("Tried to remove Already crawled from lhs - " + lhs.size()); // Should check size after already crawled are removed.
-        leftToCrawlUrls.clear();
-        leftToCrawlUrls.addAll(lhs);  // The left to be crawled list is created or added to.
-        System.out.println("This many URLs are in the queue to be crawled " +leftToCrawlUrls.size());
-        System.out.println("Running Total of All URLs on Domain after the last was removed. " + leftToCrawlUrls); // This prints out the list of all found URLs so far on the domain.
-        leftToCrawlUrls.remove(0);
-        System.out.println("Next URL to crawl - " + leftToCrawlUrls.poll()); // This prints out the list of all found URLs so far on the domain.
-
-        if (crawlingThisPage.equals(leftToCrawlUrls.poll())) {
-        	leftToCrawlUrls.remove(0);
-        	crawlingThisPage = leftToCrawlUrls.poll();
-            System.out.println("True Next Url to crawl - " + leftToCrawlUrls.poll()); // This prints out the next to crawl url.
-
-        }
-    	crawlingThisPage = leftToCrawlUrls.poll();
-
-	}
+//			if(!alreadyCrawledUrls.contains(webEle.getText()) && webEle.isDisplayed()){
+//				if(webEle.getAttribute("href").contains("https://www.skiutah.com")) {
+					alreadyCrawledUrls.add(webEle.getAttribute("href"));
+					System.out.println("I just added - " + webEle.getAttribute("href") + " to alreadyCrawledURLs");
+					webEle.click();
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				System.out.println("alreadyCrawledURLs has " + alreadyCrawledUrls.size() + " links in it." );
+				foundOnPage.remove(thisUrl);
+				System.out.println("foundOnPage has " + foundOnPage.size() + " links in it." );
+				
+				}
+	
 
 }
+
+/*	The queue is all the URLs that needs to be visited.  The list has all the URLs that been visited.  
+
+The while loop would be:
+While queue != empty{
+driver.findElements(find all anchor tags),
+Process the list elements.
+}
+
+ProcessElements(){
+for i=0, i < webelements.size(), i++{
+if (!alreadyListed.containsKey(element[i].getURL){
+  alreadyLIsted.put(url);
+queue.add(url)  
+}
+}
+
+}
+
 
 /** AUTOMATION CHALLENGE 6 (CRAWLER):
 Write a crawler that will automatically navigate to every page on the site.
