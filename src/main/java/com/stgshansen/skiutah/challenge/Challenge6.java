9368076b6119b357384642f923d00f38508bd526
scott.hansen <scott.hansen@stgconsulting.com>
2018-05-10 15:37:19 -0600

    Getting there and know where I'm going.

--- src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
+++ src/main/java/com/stgshansen/skiutah/challenge/Challenge6.java
@@ -1,10 +1,10 @@
 package com.stgshansen.skiutah.challenge;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.PriorityQueue;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,68 +19,44 @@ public class Challenge6 {
 	static ArrayList <String> alreadyCrawledUrls = new ArrayList <String>();
 	static WebElement webEle = null;
 	static List<WebElement> webEles;
-	static LinkedHashSet<WebElement> SortedList = new LinkedHashSet<WebElement>();
-	static LinkedHashSet<String> SortedUrls = new LinkedHashSet<String>();
-	static int index = 0;
 	
-	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) throws Exception {
 	    System.setProperty("webdriver.gecko.driver", "src\\geckodriver.exe");
 	    FirefoxDriver driver = new FirefoxDriver();
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebDriverWait wait = new WebDriverWait(driver, 3);
 		
 	    //Opening SkiUtah.com
 	    driver.get(homePage);
 	    foundOnPage.add(homePage);
-	    SortedUrls.add(homePage);
-		alreadyCrawledUrls.add(homePage);
+//		alreadyCrawledUrls.add(homePage);
 
 		
-	    while (foundOnPage.size() <= SortedUrls.size() && SortedUrls.size() > 0) {
-	    	webEles = driver.findElements(By.tagName("a"));
-			for(WebElement webEle : webEles) {	
-				if(webEle.getAttribute("href").contains("https://www.skiutah.com") && webEle.isDisplayed()) {
-					thisUrl = webEle.getAttribute("href");
-//			    	System.out.println("thisUrl is --- " + thisUrl);
-				}
-				SortedUrls.add(thisUrl);
-			}
-	    	System.out.println("SortedUrls --- " + SortedUrls);
-			foundOnPage.addAll(SortedUrls);
-			SortedUrls.addAll(foundOnPage);
-			foundOnPage.removeAll(foundOnPage);
-			foundOnPage.addAll(SortedUrls);
+	    while ((thisUrl = foundOnPage.poll()) != null) {
+	    	
+	    	
+			System.out.println("foundOnPage is now " + foundOnPage.size() + " size");
 			
-	   
-	    foundOnPage.remove(homePage);
-	    SortedUrls.remove(homePage);
-    	System.out.println("SortedUrls --- " + SortedUrls);
-		System.out.println("Size of SortedUrls " + SortedUrls.size());
-		System.out.println("foundOnPage is now " + foundOnPage.size() + " size");
-		System.out.println("foundOnPage ******* " + foundOnPage);
-		foundOnPage.remove(alreadyCrawledUrls);
-
-		goToUrl(alreadyCrawledUrls, foundOnPage, wait, driver);
-		
-	    }	
+			processAllUrls(thisUrl, wait, driver);
+	    }
+	    
+		System.out.println("alreadyCrawledURLs has " + alreadyCrawledUrls.size() + " links in it." );
+	    
 	    driver.close();
 	    	
 	 }
 
 
-
-	@SuppressWarnings("unlikely-arg-type")
-	private static void goToUrl(ArrayList<String> alreadyCrawledUrls, PriorityQueue<String> foundOnPage, 
-			WebDriverWait wait, FirefoxDriver driver) {
-//		if(webElement button)
-		for(WebElement webEle : driver.findElements(By.tagName("a"))) {	
-//			wait.until(driver.findElementsByTagName() isTrue)
-			foundOnPage.remove(alreadyCrawledUrls);
-			System.out.println("I am now going to this page - " + webEle.getAttribute("href"));
+	private static void processAllUrls(String thisUrl, WebDriverWait wait, FirefoxDriver driver) {
+  
+		webEles = driver.findElements(By.tagName("a"));
+		System.out.println("webEles is now " + webEles.size() + " size");
+		for(WebElement webEle : webEles) {	
+//			System.out.println("I am now going to this page - " + webEle.getAttribute("href"));
 			thisUrl = webEle.getAttribute("href");
+		    foundOnPage.add(thisUrl);
+			System.out.println("foundOnPage is now ********** " + foundOnPage);
 
-			if(webEle.getAttribute("href").contains("https://www.skiutah.com") && webEle.isDisplayed() 
-					&& !alreadyCrawledUrls.contains(thisUrl)) {
+		if(thisUrl.contains("https://www.skiutah.com") && webEle.isDisplayed() && !alreadyCrawledUrls.contains(thisUrl)) {
 				Actions action = new Actions(driver);
 				action.moveToElement(webEle).build().perform();
 				try {
@@ -89,26 +65,31 @@ public class Challenge6 {
 					e1.printStackTrace();
 				}
 
-//			if(!alreadyCrawledUrls.contains(webEle.getText()) && webEle.isDisplayed()){
-//				if(webEle.getAttribute("href").contains("https://www.skiutah.com")) {
-					alreadyCrawledUrls.add(webEle.getAttribute("href"));
-					System.out.println("I just added - " + webEle.getAttribute("href") + " to alreadyCrawledURLs");
-					webEle.click();
+				alreadyCrawledUrls.add(thisUrl);
+				System.out.println("I just added - " + thisUrl + " to alreadyCrawledURLs");
+				webEle.click();
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e1) {
+						if(driver.switchTo().alert() != null)
+						{
+						    Alert alert = driver.switchTo().alert();
+						    alert.dismiss(); // alert.accept();
+						}
+	
 						e1.printStackTrace();
-					}
-				System.out.println("alreadyCrawledURLs has " + alreadyCrawledUrls.size() + " links in it." );
-				System.out.println(alreadyCrawledUrls);
+						}
 				foundOnPage.remove(thisUrl);
 				System.out.println("foundOnPage has " + foundOnPage.size() + " links in it." );
-				
+
+//				foundOnPage.remove(alreadyCrawledUrls);
+		}
+		}
 				return;
-				}
+				
 		}
-		}
-}
+	}
+
 
 /*	The queue is all the URLs that needs to be visited.  The list has all the URLs that been visited.  
 
